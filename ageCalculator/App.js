e7c9e9818baba9f260e1a5702e9820c98b40781c
vamsisollem <50356552+vamsisollem@users.noreply.github.com>
2023-12-26 15:44:17 -0500

    Add files via upload

---
+++ ageCalculator/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AgeCard from './components/AgeCard';
+function App() {
+  return (
+    <>
+      <AgeCard />
+    </>
+  );
+}
+
+export default App;
