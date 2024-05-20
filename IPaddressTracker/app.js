10c111b674314b47ea43cdc4688653756c25a7e9
vamsisollem <50356552+vamsisollem@users.noreply.github.com>
2024-05-20 14:18:16 -0400

    Add files via upload

---
+++ IPaddressTracker/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const fs= require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+// const apiData = JSON.parse(fs.readFileSync('data.json','utf-8'));
+app.get('/', (req,res)=>{
+    res.render('data', {});
+});
+app.listen(3000);
