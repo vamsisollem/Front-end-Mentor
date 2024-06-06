59f1cbb5150990e15cf9839e94612190d3aa15c8
vamsisollem <50356552+vamsisollem@users.noreply.github.com>
2024-06-05 21:18:41 -0400

    new version

--- ExpensesChart/app.js
+++ ExpensesChart/app.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
-app.get('/',(req, res)=>{
-    res.render('data',{});
+app.get('/',(req,res)=>{
+    res.render('user');
+})
+app.post('/expenditure', async function(req, res) {
+    const data = req.body;
+    const formatData = [
+        {
+            "day":'mon',
+            "amount":data.monday
+        },{
+            "day":'tue',
+            "amount":data.tuesday
+        },{
+            "day":'wed',
+            "amount":data.wednesday
+        },
+        {
+            "day":'thu',
+            "amount":data.thursday
+        },
+        {
+            "day":'fri',
+            "amount":data.friday
+        },
+        {
+            "day":'sat',
+            "amount":data.saturday
+        },{
+            "day":'sun',
+            "amount":data.sunday
+        }
+    ];
+    const expense = JSON.stringify(formatData);
+    fs.writeFileSync("data.json", expense);
+    const jsonData = JSON.parse(fs.readFileSync('data.json','utf-8'));
+    res.render('data', {jsonData});
 });
+
 app.listen(3000);
